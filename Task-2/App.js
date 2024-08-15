0cb5154b52000fda49930aa5e4d6f9e849ae90c6
Suganthi-A <asuganthi387@gmail.com>
2024-08-15 09:37:55 +0530

    Task2

---
+++ Task-2/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './style.css';
+import Task2 from './Components/Task2';
+
+function App() {
+  return (
+    
+   <>
+    <Task2/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ Task-2/Task2.js
@@ -0,0 +1,22 @@
+import React from "react";
+import moon from "../Images/rcb.webp";
+import Img from '../Images/MI.png';
+
+function Task2(){
+return(
+
+    <>
+  
+  <div className="Container">
+    <div className="insideCard">
+        <h1>Welcome</h1>
+      <img src={moon} className="Suganthi"/>
+      <img src={Img} className="Sowmiya"/>
+    </div>
+  </div>
+
+    </>
+)
+
+}
+export default Task2;
