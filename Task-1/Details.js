9fcedbad21c5d20ea619f0354978f6ff7c4b3e0e
Suganthi-A <asuganthi387@gmail.com>
2024-08-15 09:35:35 +0530

    Task1

---
+++ Task-1/Details.js
@@ -0,0 +1,15 @@
+
+import logo from './logo.svg';
+import '../src/app.css';
+import Task1 from './Components/Task1';
+
+function App() {
+  return (
+    <>
+    <Task1/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ Task-1/Task-1.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Img from '../Images/sarath.jpg';
+function Task1(){
+return(
+  <>
+  <div className="Container">
+    <h1>Congratulations</h1>
+    <div className="insideCard">
+    <img src={Img} className="sarath"/>
+     
+    <h1> Suganthi</h1>
+    <p>BSC Information Technology</p>
+    <p>KG college of arts and science</p>
+    <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+ </>
+)
+
+}
+export default Task1;
