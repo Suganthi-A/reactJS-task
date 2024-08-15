ec3b0b5ad8a742411c1c1277fe58e71e2c171175
Suganthi-A <asuganthi387@gmail.com>
2024-08-15 09:39:23 +0530

    Task3

---
+++ Task-3/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './style1.css';
+
+import Task3 from './Components/Task3';
+
+function App() {
+  return (
+    
+   
+    <>
+    <Task3/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ Task-3/Task3.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Task3(){
+    return(
+        <div>
+            <h1>Hello</h1>
+            <button type="button"  className="btn btn-primary-m5">Login</button>
+            <button type="button" className="btn1 btn-success-m5">Output</button>
+            <button type="button" className="btn2 btn-warning-m5">Display</button>
+            </div>
+            
+    )
+}
+
+export default Task3;
